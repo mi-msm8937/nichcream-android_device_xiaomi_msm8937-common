1140d1281b67e7e61b5a08aa5832c67f5f43d012
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-04-14 03:19:28 +0800

    msm8937-common: libinit: Invoke extras libinit if exists

--- libinit/Android.mk
+++ libinit/Android.mk
@@ -29,5 +29,12 @@ LOCAL_C_INCLUDES := \
 
 LOCAL_SRC_FILES := init_xiaomi_msm8937.cpp
 LOCAL_WHOLE_STATIC_LIBRARIES := libinit_xiaomi_msm8937
+
+ifneq ($(wildcard vendor/extras/libinit/Android.mk),)
+LOCAL_CFLAGS += -DUSE_EXTRAS
+LOCAL_HEADER_LIBRARIES += libinit_extras_headers
+LOCAL_WHOLE_STATIC_LIBRARIES += libinit_extras
+endif
+
 LOCAL_MODULE := init_xiaomi_msm8937
 include $(BUILD_STATIC_LIBRARY)
--- libinit/init_xiaomi_msm8937.cpp
+++ libinit/init_xiaomi_msm8937.cpp
@@ -6,8 +6,15 @@
 
 #include <libinit_dalvik_heap.h>
 
+#ifdef USE_EXTRAS
+#include <libinit_extras.h>
+#endif
+
 #include "vendor_init.h"
 
 void vendor_load_properties() {
     set_dalvik_heap();
+#ifdef USE_EXTRAS
+    load_extras();
+#endif
 }
