ProjectArcana-Devices/android_device_xiaomi_msm8937-common

msm8937-common: libinit: Allow devices to use theirs own init

BoardConfigCommon.mk
@@ -111,8 +111,8 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):init_xiaomi_msm8937
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_msm8937
+TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_msm8937
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_msm8937
 
 # Partitions
 TARGET_COPY_OUT_VENDOR := vendor

mi-msm8937/nichcream-android_device_xiaomi_msm8937-common

BoardConfigCommon.mk
@@ -54,9 +54,9 @@ TARGET_BOOTLOADER_BOARD_NAME 	:= msm8937
 TARGET_NO_BOOTLOADER 		:= true
 
 # Init
-TARGET_INIT_VENDOR_LIB         := //$(VENDOR_PATH):libinit_xiaomi_msm8937
+TARGET_INIT_VENDOR_LIB         ?= //$(VENDOR_PATH):libinit_xiaomi_msm8937
 TARGET_PLATFORM_DEVICE_BASE    := /devices/platform/soc/
-TARGET_RECOVERY_DEVICE_MODULES := libinit_xiaomi_msm8937
+TARGET_RECOVERY_DEVICE_MODULES ?= libinit_xiaomi_msm8937
 
 # Properties
 TARGET_SYSTEM_PROP := $(COMMON_PATH)/system.prop