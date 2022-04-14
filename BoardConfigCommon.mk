4400bbd38567e653352b9f1b03d7e4bef03feca4
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-04-14 23:48:22 +0800

    msm8937-common: Enable System-as-root
    To make device boot with this, Delete system node from dts fstab

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,7 +21,7 @@ COMMON_PATH := device/xiaomi/msm8937-common
 # Kernel
 BOARD_KERNEL_BASE		:= 0x80000000
 BOARD_KERNEL_CMDLINE 		:= androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
-BOARD_KERNEL_CMDLINE 		+= firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true
+BOARD_KERNEL_CMDLINE 		+= firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true ro root=/dev/mmcblk0p24 rootwait skip_initramfs init=/init
 BOARD_KERNEL_IMAGE_NAME 	:= Image.gz-dtb
 BOARD_KERNEL_PAGESIZE 		:=  2048
 BOARD_MKBOOTIMG_ARGS 		:= --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
@@ -206,6 +206,7 @@ BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
 MALLOC_SVELTE := true
 
 # Partitions
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 TARGET_COPY_OUT_VENDOR := vendor
 ifneq ($(TARGET_DEVICE),tiare)
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
